12d49e469613ff010ad638c7f78cab6ea125eab8
bm moon <gomi6612@gmail.com>
2025-01-10 15:08:49 +0900

    문병무: 엔진 업데이트

---
+++ EngineCore/EngineCore.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <EngineBase/EngineDefine.h>
+#include <EngineBase/EngineTimer.h>
+#include <EngineBase/EngineString.h>
+#include <EnginePlatform/EngineWindow.h>
+#include "EngineGraphicDevice.h"
+#include "IContentsCore.h"
+#include "Level.h"
+#include <memory>
+
+
+// 설명 :
+class UEngineCore
+{
+public:
+	// constrcuter destructer
+	ENGINEAPI UEngineCore();
+	ENGINEAPI virtual ~UEngineCore() = 0;
+
+	ENGINEAPI static void EngineStart(HINSTANCE _Instance, std::string_view _DllName);
+
+	template<typename GameModeType, typename MainPawnType>
+	static class std::shared_ptr<class ULevel> CreateLevel(std::string_view _Name)
+	{
+		std::string UpperString = UEngineString::ToUpper(_Name);
+
+		// 1 유지하고 있겠죠.
+		// shared_ptr을 사용하므로 new UEngineLevel()
+		std::shared_ptr<ULevel> NewLevel = NewLevelCreate(UpperString);
+		// std::make_shared
+		// new UEngineLevel();
+
+		std::shared_ptr<GameModeType> GameMode = NewLevel->SpawnActor<GameModeType>();
+		std::shared_ptr<MainPawnType> Pawn = NewLevel->SpawnActor<MainPawnType>();
+
+		NewLevel->InitLevel(GameMode.get(), Pawn.get());
+
+		// 2가 됩니다
+		return NewLevel;
+	}
+
+	ENGINEAPI static void OpenLevel(std::string_view _Name);
+
+
+	ENGINEAPI static FVector GetScreenScale();
+
+	ENGINEAPI static UEngineGraphicDevice& GetDevice();
+
+	ENGINEAPI static UEngineWindow& GetMainWindow();
+
+	ENGINEAPI static std::map<std::string, std::shared_ptr<class ULevel>> GetAllLevelMap();
+
+protected:
+
+private:
+	ENGINEAPI static UEngineWindow MainWindow;
+
+	ENGINEAPI static UEngineGraphicDevice Device;
+	// 데이터영역에 있죠? => 언제 삭제될까요?
+	// 릭체크는 
+	static HMODULE ContentsDLL;
+	static std::shared_ptr<IContentsCore> Core;
+	static UEngineInitData Data;
+
+	static UEngineTimer Timer;
+
+	static void WindowInit(HINSTANCE _Instance);
+	static void LoadContents(std::string_view _DllName);
+
+	static void EngineFrame();
+	static void EngineEnd();
+
+	ENGINEAPI static std::shared_ptr<ULevel> NewLevelCreate(std::string_view _Name);
+
+	static std::map<std::string, std::shared_ptr<class ULevel>> LevelMap;
+	static std::shared_ptr<class ULevel> CurLevel;
+	static std::shared_ptr<class ULevel> NextLevel;
+};
+
---
+++ EngineCore/SceneComponent.h
@@ -0,0 +1,125 @@
+#pragma once
+#include "ActorComponent.h"
+
+// #include "PreCompile.h"
+
+// 설명 :
+class USceneComponent : public UActorComponent
+{
+	friend class AActor;
+
+	// Local 부모영향 받음
+	// World 안받음
+	// 아무것도 없음 안받음
+
+public:
+	// constrcuter destructer
+	USceneComponent();
+	virtual ~USceneComponent() = 0;
+
+	// delete Function
+	USceneComponent(const USceneComponent& _Other) = delete;
+	USceneComponent(USceneComponent&& _Other) noexcept = delete;
+	USceneComponent& operator=(const USceneComponent& _Other) = delete;
+	USceneComponent& operator=(USceneComponent&& _Other) noexcept = delete;
+
+	ENGINEAPI void ComponentTick(float _DeltaTime) override;
+
+	void AddRelativeLocation(const FVector& _Value)
+	{
+		Transform.Location += _Value;
+		TransformUpdate();
+	}
+
+	void SetWorldLocation(const FVector& _Value)
+	{
+		IsAbsolute = true;
+		Transform.Location = _Value;
+		TransformUpdate();
+	}
+
+	void AddWorldLocation(const FVector& _Value)
+	{
+		IsAbsolute = true;
+		Transform.Location += _Value;
+		TransformUpdate();
+	}
+
+	void SetRelativeLocation(const FVector& _Value)
+	{
+		Transform.Location = _Value;
+		TransformUpdate();
+	}
+
+
+	void AddWorldRotation(const FVector& _Value)
+	{
+		IsAbsolute = true;
+		Transform.Rotation += _Value;
+		TransformUpdate();
+	}
+
+	void AddLocalRotation(const FVector& _Value)
+	{
+		Transform.Rotation += _Value;
+		TransformUpdate();
+	}
+
+	void SetRotation(const FVector& _Value)
+	{
+		Transform.Rotation = _Value;
+		TransformUpdate();
+	}
+
+	void SetScale3D(const FVector& _Value)
+	{
+		IsAbsolute = true;
+		Transform.Scale = _Value;
+		TransformUpdate();
+	}
+
+	// local
+	void SetRelativeScale3D(const FVector& _Value)
+	{
+		// 절대값이라는 뜻
+		// 이게 true가 되면 부모가 있건 없건
+		// 100 100 100
+		// 10 10 10
+		// 나는 무조건 이값에 해당하는 행렬이 되어야 한다는 뜻으로 
+		Transform.Scale = _Value;
+		Transform.Scale.W = 0.0f;
+		TransformUpdate();
+	}
+
+	FVector GetWorldScale3D()
+	{
+		return Transform.WorldScale;
+	}
+
+
+	FTransform& GetTransformRef()
+	{
+		return Transform;
+	}
+
+
+	ENGINEAPI void SetupAttachment(std::shared_ptr<USceneComponent> _Parent);
+
+	void SetupAttachment(USceneComponent* _Parent);
+
+	ENGINEAPI void TransformUpdate();
+
+protected:
+	bool IsAbsolute = false;
+
+	FTransform Transform;
+
+	ENGINEAPI void BeginPlay() override;
+
+	void ParentMatrixCheck();
+
+private:
+	USceneComponent* Parent = nullptr;
+	std::list<std::shared_ptr<USceneComponent>> Childs;
+};
+
