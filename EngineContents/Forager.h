a94b1c52ba7bee465a1b86b3379b4555d72abc70
bm moon <gomi6612@gmail.com>
2025-01-10 15:41:00 +0900

    문병무: 업데이트

--- EngineContents/Forager.h
+++ EngineContents/Forager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <EngineCore/Actor.h>
+#include <EngineCore/SpriteRenderer.h>
+#include <EnginePlatform/EngineWinImage.h>
 
 // Ό³Έν :
 class AForager : public AActor
@@ -15,9 +17,22 @@ public:
 	AForager& operator=(const AForager& _Other) = delete;
 	AForager& operator=(AForager&& _Other) noexcept = delete;
 
+	std::shared_ptr<class USpriteRenderer> GetRenderer()
+	{
+		return Renderer;
+	}
+
 protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
 
 private:
+	std::shared_ptr<class UTimeEventComponent> TimeEventComponent;
+
+
+	std::shared_ptr<class USpriteRenderer> Renderer;
+
+	std::shared_ptr<class UCollision> Collision;
 
 };
 
